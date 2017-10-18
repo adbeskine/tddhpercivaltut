6d23f34ce77186fcfaa5ff1ae2955600b5ec2257
Alexander Beskine <a.d.beskine@outlook.com>
2017-10-19 00:04:15 +0100

    home page only saves new item when necesarry, appropriate unit tests added, about to refractor unit tests

--- lists/tests.py
+++ lists/tests.py
@@ -15,8 +15,13 @@ class HomePageTest(TestCase):
 
 	def test_can_save_a_POST_request(self):
 		response = self.client.post('/', data={'item_text':'A new list item'})
-		self.assertIn('A new list item', response.content.decode())
-		self.assertTemplateUsed(response, 'lists/home.html')
+
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
 
 class ItemModelTest(TestCase):
 
@@ -37,6 +42,9 @@ class ItemModelTest(TestCase):
 		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
 		self.assertEqual(second_saved_item.text, 'Item the second')
 
+	def test_only_saves_items_when_necessar(self):
+		self.client.get('/')
+		self.assertEqual(Item.objects.count(), 0)
 if __name__ == '__main__':
 	unittest.main()
 
