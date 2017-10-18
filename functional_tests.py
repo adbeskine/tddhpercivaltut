ef5bc3bbd845a59035f0f5958a16c0ccb8144c99
Alexander Beskine <a.d.beskine@outlook.com>
2017-10-18 22:21:23 +0100

    tiny refractor sucess in FTs

--- functional_tests.py
+++ functional_tests.py
@@ -18,6 +18,15 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	#-----HELPER METHODS-----#
+
+	def check_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
+
+
 	def test_can_start_a_list_and_retrive_it_later(self):
 
 		# Alex has heard about a cool new online to-do app. He goes to check out its homepage
@@ -42,9 +51,7 @@ class NewVisitorTest(unittest.TestCase):
 		input_box.send_keys(Keys.ENTER)
 		time.sleep(1)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: buy peacock feathers', [row.text for row in rows])
+		self.check_row_in_list_table('1: buy peacock feathers')
 	
 		# There is still a text box inviting her to add another item. She enters "Use peacock feathers to make a fly"
 		input_box = self.browser.find_element_by_id('id_new_item')
@@ -56,8 +63,8 @@ class NewVisitorTest(unittest.TestCase):
 		table = self.browser.find_element_by_id('id_list_table')
 		rows= table.find_elements_by_tag_name('tr')
 		time.sleep(1)
-		self.assertIn("1: buy peacock feathers", [row.text for row in rows])
-		self.assertIn("2: use peacock feathers to make a fly", [row.text for row in rows])
+		self.check_row_in_list_table('1: buy peacock feathers')
+		self.check_row_in_list_table('2: use peacock feathers to make a fly')
 	
 	# Edith wonders whether the site will remember her list. Then she sees that
 	# the site has generated a unique URL for her -- there is some explanatory text to that effect.
