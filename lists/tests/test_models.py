86629620c21e59dd5fa6b65b30fb310077d9c96a
Alexander Beskine <a.d.beskine@outlook.com>
2017-10-26 10:28:33 +0100

    use get_abslute_url on List model to DRY urls in views

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -42,6 +42,11 @@ class ListAndItemModelTest(TestCase):
 			item.save()
 			item.full_clean()
 
+	def test_get_absolute_url(self):
+		list_ = List.objects.create()
+		self.assertEqual(list_.get_absolute_url(), f'/lists/{list_.id}/')
+		
+
 
 
 if __name__ == '__main__':
